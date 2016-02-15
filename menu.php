b5f8310f16e3529fc1e14369ab36be335cc009f2
alexander.wine <alexander.wine@smail.rasmussen.edu>
2016-02-14 19:07:28 -0500

    Updated Menu Structure

--- menu.php
+++ menu.php
@@ -11,10 +11,11 @@
       if(!isset($_SESSION['user'])) {
               
       } else {
-        echo "<li><a href='/logout.php'>Logout</a></li>";
-        if($_SESSION['access'] > 1) {
+         if($_SESSION['access'] > 1) {
           echo "<li><a href='/grocery_crud/index.php/welcome/employees'>Database</a></li>";
         }
+        echo "<li><a href='/logout.php'>Logout</a></li>";
+       
       }
    ?>
       
